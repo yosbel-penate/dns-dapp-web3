0d866718f583d354bfecc8c216c1750e4f3d363f
Пеньяте Барсело Йосбель - <m1911770@edu.misis.ru>
2022-02-19 23:23:28 +0300

    add offert contract

--- offert.sol
+++ offert.sol
@@ -1,10 +1,13 @@
 pragma solidity ^0.8.7;
 contract OffertDNS{
-    address payable public owner;
+
+    address public owner;
     mapping(address=>string) private UrlNameByUser;
-    constructor()payable{
-        owner = payable(msg.sender);
+
+    constructor(){
+        owner = msg.sender;
     }
+
     function setURL(string memory _urlName)external{
         UrlNameByUser[msg.sender]=_urlName;
     }
